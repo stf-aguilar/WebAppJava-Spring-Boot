8c8eb201e198d421a855ee283d717975758d0daa
Estefania Aguilar <silestagui@gmail.com>
2020-07-14 08:14:15 -0300

    creación de un proyecto web con Spring Boot

---
+++ spring-boot-web/src/test/java/com/bolsadeideas/springboot/app/web/SpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.app.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
