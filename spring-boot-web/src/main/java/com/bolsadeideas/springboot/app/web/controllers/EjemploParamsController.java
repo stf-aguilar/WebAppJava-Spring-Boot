70e646ff3bd600d674ef77dfdfc3ff7e298ab305
Estefania Aguilar <silestagui@gmail.com>
2020-07-30 19:20:29 -0300

    request param

---
+++ spring-boot-web/src/main/java/com/bolsadeideas/springboot/app/web/controllers/EjemploParamsController.java
@@ -0,0 +1,31 @@
+package com.bolsadeideas.springboot.app.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/params")
+public class EjemploParamsController {
+	
+	@GetMapping("/")
+	public String index() {
+		return "params/index";
+	}
+	
+	@GetMapping("/string")
+	public String param(@RequestParam(name="texto") String texto, Model model) {
+		model.addAttribute("resultado", "El texto enviado es: "+texto);
+		return "params/ver";
+	}
+	
+	@GetMapping("/mix-params")
+	public String param(@RequestParam String saludo, @RequestParam Integer numero, Model model) {
+		model.addAttribute("resultado","El saludo enviado es: '"+saludo+"' y el número es '" + numero + "'");
+		return "params/ver";
+	}
+		
+	
+}
--- spring-boot-web/src/main/java/com/bolsadeideas/springboot/app/web/controllers/IndexController.java
+++ spring-boot-web/src/main/java/com/bolsadeideas/springboot/app/web/controllers/IndexController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.bolsadeideas.springboot.app.web.models.Usuario;
@@ -35,14 +36,18 @@ public class IndexController {
 	
 	@RequestMapping("/listar")
 	public String listar(Model model) {
+		model.addAttribute("titulo", "Listado de usuarios");
+		
+		return "listar";
+	}
+	
+	@ModelAttribute("usuarios")
+	public List<Usuario> poblarUsuarios(){
 		List<Usuario> usuarios = new ArrayList<>();
 		usuarios.add(new Usuario("Estefanía","Aguilar","silestagui@gmail.com"));
 		usuarios.add(new Usuario("Pedro","Picapiedras","pp@gmail.com"));
 		usuarios.add(new Usuario("Mujer","Maravilla","mujerm@hotmail.com"));
-		model.addAttribute("titulo", "Listado de usuarios");
-		model.addAttribute("usuarios", usuarios);
 		
-		return "listar";
-		
+		return usuarios;
 	}
 }
