891ea43ca570b1397eb03d1f90f485df5d1ac61e
Estefanía <silestagui@gmail.com>
2020-09-03 16:07:58 +0200

    mensajes de errores en la vista del formulario

--- spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormControlller.java
+++ spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormControlller.java
@@ -1,8 +1,15 @@
 package com.bolsadeideas.springboot.form.app.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -13,18 +20,28 @@ public class FormControlller {
 
 	@GetMapping("/form")
 	public String form(Model model) {
+		Usuario usuario = new Usuario();
 		model.addAttribute("titulo", "Formulario usuarios");
-		
+		model.addAttribute("user", usuario);
 		return "form";
 	}
 	
 	
 	@PostMapping("/form")
-	public String procesar(Usuario usuario, Model model) {
+	public String procesar(@Valid @ModelAttribute("user") Usuario usuario,BindingResult result, Model model) {
 		
 		model.addAttribute("titulo", "Resultado form");
-		model.addAttribute("usuario", usuario);
 		
+		if(result.hasErrors()) {
+			Map<String, String> errores = new HashMap<>();
+			result.getFieldErrors().forEach(err->{
+				errores.put(err.getField(), "El campo ".concat(err.getField()).concat("").concat(err.getDefaultMessage()));
+			});
+			model.addAttribute("error", errores);
+			return "form";
+		}
+		
+		model.addAttribute("usuario", usuario);
 		return "resultado";
 	}
 }
--- spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/models/Usuario.java
+++ spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/models/Usuario.java
@@ -1,8 +1,13 @@
 package com.bolsadeideas.springboot.form.app.models;
 
+import javax.validation.constraints.NotEmpty;
+
 public class Usuario {
+	@NotEmpty
 	private String username;
+	@NotEmpty
 	private String password;
+	@NotEmpty
 	private String email;
 	
 	public String getUsername() {
